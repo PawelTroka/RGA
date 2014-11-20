459ca5851a681ecf3dc52aa9afaeb5ed6c18c0ba
PTroka <pawel.troka@outlook.com>
2014-11-20 20:14:03 +0100

    moved to Visual Studio

---
+++ RGA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RGA.Startup))]
+namespace RGA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
