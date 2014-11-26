7687e5ab24c121251f838f4c7dfe988cc7f4e077
PTroka <pawel.troka@outlook.com>
2014-11-26 20:17:10 +0100

    Poprawki w generatorze trasy, refaktoryzacja kodu, dodatki w modelu trasy

--- RGA/Helpers/RouteGenerator.cs
+++ RGA/Helpers/RouteGenerator.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using Google.Maps;
+using Google.Maps.DistanceMatrix;
 using Google.Maps.Elevation;
 using GoogleMapsApi;
 using GoogleMapsApi.Entities.Directions.Request;
 using GoogleMapsApi.Entities.Directions.Response;
 using RGA.Models;
+using WebGrease.Css.Extensions;
+using Route = RGA.Models.Route;
 using TravelMode = Google.Maps.TravelMode;
 
 namespace RGA.Helpers
 {
     public class RouteGenerator
     {
+        private Route route;
         private TspSolver tspSolver;
-
-        //private GoogleMapsApi.Entities.Common. googleMaps;
         private string browserAPIKey = "AIzaSyCBup_64x3U1wfm3NTiu1mO_8tyz1GAM6Q";
-
         private string serverAPIKey = "AIzaSyDPhoaf5psgGK27m6FWKTyLiTf-aIiHiWs";
         public RouteGenerator()
         {
-            Addresses =  new List<string>();
-         //   googleMaps = new GoogleMaps();
-            
+            Addresses = new List<string>();
+            route = new RGA.Models.Route();
         }
 
         public RouteOptimizationProvider routeOptimizationProvider { get; set; }
@@ -34,109 +35,153 @@ namespace RGA.Helpers
         public string BaseAddress { get; set; }
         public List<string> Addresses { get; set; }
 
-
-        
         public RGA.Models.Route GenerateRoute()
         {
+            if (routeOptimizationProvider == RouteOptimizationProvider.RGA)
+                SortThingsAccordingToCost();
+
+            var directions = getDirections();
+
+            route.Summary = directions.Routes.First().Summary;
+            route.Sections = new List<Leg>(directions.Routes.First().Legs);
+
+            if (routeOptimizationProvider == RouteOptimizationProvider.GoogleMaps)
+                SortThingsAccordingToWaypointOrder(directions.Routes.First().WaypointOrder);
+
+            directions.Routes.First().Legs.ForEach(leg => route.Duaration += leg.Duration.Value);
+            directions.Routes.First().Legs.ForEach(leg => route.Distance += leg.Distance.Value);
+
+            route.Addresses = Addresses;
+            route.StartAddress = BaseAddress;
+            route.Image = getImageBytes();
+
+            return route;
+        }
+
+        private void SortThingsAccordingToCost()
+        {
+            var response = getDistanceMatrix();
+
+            var costs = new double[Addresses.Count + 1, Addresses.Count + 1];
+            var indices = new int[Addresses.Count + 1];
+
+            for (int i = 0; i < response.Rows.Length; i++)
+            {
+                indices[i] = i;
+                for (int j = 0; j < response.Rows[i].Elements.Length; j++)
+                {
+                    if (routeOptimizationType == RouteOptimizationType.Time)
+                        costs[i, j] = long.Parse(response.Rows[i].Elements[j].duration.Value);
+
+                    else if (routeOptimizationType == RouteOptimizationType.Distance)
+                        costs[i, j] = long.Parse(response.Rows[i].Elements[j].distance.Value);
+                }
+
+            }
+
+            tspSolver = new HeldKarpTSPSolver(indices, costs);
+
+            double cost;
+
+            var optimalRoute = tspSolver.Solve(out cost);
+
+            var tmpAddress = new List<string>();
+
+            foreach (var item in optimalRoute)
+            {
+                if (item != 0)
+                    tmpAddress.Add(Addresses[item - 1]);
+            }
+            Addresses = tmpAddress;
+        }
+
+        private void SortThingsAccordingToWaypointOrder(int[] order)
+        {
+            Addresses.Sort((a1, a2) => order[Addresses.IndexOf(a1)].CompareTo(order[Addresses.IndexOf(a2)]));
+
+            var orderOfSections = new List<int> { 0 };
+            orderOfSections.AddRange(order);
+            for (int i = 1; i < orderOfSections.Count; i++)
+                orderOfSections[i]++;
+            orderOfSections.Add(0);
+            route.Sections.Sort(
+                (s1, s2) => orderOfSections[route.Sections.IndexOf(s1)].CompareTo(orderOfSections[route.Sections.IndexOf(s2)]));
+        }
+
+
+        private DistanceMatrixResponse getDistanceMatrix()
+        {
+            var waypoints = new SortedList<int, Google.Maps.Waypoint> { { 0, new Waypoint() { Address = BaseAddress } } };
+
+            for (int i = 0; i < Addresses.Count; i++)
+            {
+                waypoints.Add(i + 1, new Waypoint() { Address = Addresses[i] });
+            }
+
+            var request = new Google.Maps.DistanceMatrix.DistanceMatrixRequest()
+            {
+                Language = "pl",
+                Avoid = Avoid.tolls, //| Avoid.ferries,
+                WaypointsOrigin = waypoints,
+                WaypointsDestination = waypoints,
+                Units = Units.metric,
+                Mode = TravelMode.driving
+            };
+
+
+            var response = new Google.Maps.DistanceMatrix.DistanceMatrixService().GetResponse(request);
+
+            if (response.Status != ServiceResponseStatus.Ok)
+                throw new Exception("Nie udało się wygenerować trasy!\nPowód: " + response.Status.ToString());
+
+            return response;
+        }
+
+        private DirectionsResponse getDirections()
+        {
+            var directionsRequest = new DirectionsRequest()
+            {
+                Origin = BaseAddress,
+                Destination = BaseAddress,
+                Waypoints = Addresses.ToArray(),
+                Language = "pl",
+                Avoid = AvoidWay.Tolls,
+            };
 
 
             if (routeOptimizationProvider == RouteOptimizationProvider.GoogleMaps)
+                directionsRequest.OptimizeWaypoints = true; //solves TSP for us, but is limited to something like 8 points
+
+            DirectionsResponse directions = GoogleMaps.Directions.Query(directionsRequest);
+
+            if (directions.Status != DirectionsStatusCodes.OK)
+                throw new Exception("Nie udało się wygenerować trasy!\nPowód: " + directions.StatusStr);
+
+            return directions;
+        }
+
+
+        private byte[] getImageBytes()
+        {
+            var locations = new List<Location>() { BaseAddress };
+
+            foreach (var address in Addresses)
             {
-               // Google.Maps.Direction.DirectionRequest 
-
-               var  directionsRequest = new DirectionsRequest()
-                {
-                    Origin = BaseAddress,
-                    Destination = BaseAddress,
-                    Waypoints = Addresses.ToArray(),
-                    Language = "pl",
-                    Avoid = AvoidWay.Tolls,
-                    OptimizeWaypoints = true //solves TSP for us, but is limited to something like 8 points
-                };
-
-            
-                DirectionsResponse directions = GoogleMaps.Directions.Query(directionsRequest);
-
-                
-
-                if(directions.Status != DirectionsStatusCodes.OK)
-                    throw new Exception("Nie udało się wygenerować trasy!\nPowód: "+directions.StatusStr);
-
-
+                locations.Add(address);
             }
-            else if (routeOptimizationProvider == RouteOptimizationProvider.RGA)
+            locations.Add(BaseAddress);
+
+            var mapRequest = new Google.Maps.StaticMaps.StaticMapRequest()
             {
-                var waypoints = new SortedList<int, Google.Maps.Waypoint> {{0, new Waypoint() {Address = BaseAddress}}};
+                Language = "pl",
+                //MapType = Mapt
+                Format = GMapsImageFormats.JPG,
+                Path = new Path(locations),
+                // Size = new Size(600,600)
+            };
+            //route.Sections.First().Steps.First().
 
-                for (int i = 0; i < Addresses.Count; i++)
-                {
-                    waypoints.Add(i+1,new Waypoint(){Address = Addresses[i]});
-                }
-
-                var request = new Google.Maps.DistanceMatrix.DistanceMatrixRequest()
-                {
-                    Language = "pl",
-                    Avoid = Avoid.tolls | Avoid.ferries,
-                    WaypointsOrigin = waypoints,
-                    WaypointsDestination = waypoints,
-                    Units = Units.metric,
-                    Mode = TravelMode.driving
-                };
-
-
-                var response = new Google.Maps.DistanceMatrix.DistanceMatrixService().GetResponse(request);
-
-                if (response.Status != ServiceResponseStatus.Ok)
-                    throw new Exception("Nie udało się wygenerować trasy!\nPowód: " + response.Status.ToString());
-
-
-                var costs = new double[Addresses.Count + 1, Addresses.Count + 1];
-                var indices = new int[Addresses.Count + 1];
-
-                for (int i = 0; i < response.Rows.Length; i++)
-                {
-                    indices[i] = i;
-                    for (int j = 0; j < response.Rows[i].Elements.Length; j++)
-                    {
-                        if (routeOptimizationType == RouteOptimizationType.Time)
-                            costs[i, j] = long.Parse(response.Rows[i].Elements[j].duration.Value);
-                        
-                        else if (routeOptimizationType == RouteOptimizationType.Distance)
-                            costs[i, j] = long.Parse(response.Rows[i].Elements[j].distance.Value);
-                    }
-                    
-                }
-
-
-                tspSolver = new HeldKarpTSPSolver(indices,costs);
-
-                double cost;
-
-                var optimalRoute = tspSolver.Solve(out cost);
-
-                var route = new RGA.Models.Route();
-                
-                route.StartAddress = BaseAddress;
-
-                foreach (var item in optimalRoute)
-                {
-                    if (item != 0)
-                        route.Addresses.Add(Addresses[item - 1]);
-                    if (item < Addresses.Count + 1)
-                    {
-                        route.Duaration +=
-                            TimeSpan.FromSeconds(long.Parse(response.Rows[item].Elements[item + 1].duration.Value));
-
-                        route.Distance += long.Parse(response.Rows[item].Elements[item + 1].distance.Value);
-                    }
-                }
-
-                return route;
-
-            }
-
-
-            return new RGA.Models.Route();
+            return (new Google.Maps.StaticMaps.StaticMapService()).GetImageBytes(mapRequest);
         }
     }
 }
--- RGA/Models/Route.cs
+++ RGA/Models/Route.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using GoogleMapsApi.Entities.Directions.Response;
 
 namespace RGA.Models
 {
@@ -10,12 +11,17 @@ namespace RGA.Models
     {
 
 
-
+        public byte[] Image { get; set; } //jpeg
+        public string Summary { get; set; }
         public List<string> Notes { get; set; }
+
+        public List<Leg> Sections { get; set; }
 
         public DateTime StartDateTime { get; set; }
 
-        public ApplicationUser Driver { get; set; }
+        public ApplicationUser Driver { get; set; } //driver who drives that route
+
+        public ApplicationUser Worker { get; set; } //worker who created that route
 
         public string StartAddress { get; set; }
         public List<string> Addresses { get; set; }
